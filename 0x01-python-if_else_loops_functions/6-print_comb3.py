5aff4eaf7cce087cfb221bcdfca812efebc319a7
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-06-07 02:48:00 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for number1 in range(0, 10):
     for number2 in range(0, 10):
         if number1 == number2 or number1 > number2:
             continue
-        if number1 == 9:
-            print("{0}{1}".format(number1, number2), end=", ")
+        if number1 == 8:
+            print("{0}{1}".format(number1, number2))
         else:
             print("{0}{1}".format(number1, number2), end=", ")
