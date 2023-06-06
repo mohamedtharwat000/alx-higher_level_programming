1b890a550585da8a042a0beb5c3515f50c95339a
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-06-06 15:17:55 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,15 +7,15 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list, *head = list;
+	listint_t *head = list, *current = list->next;
 
-	while (current && current->next)
+	while (head && current)
 	{
-		current = current->next;
 		if (current == head)
 		{
 			return (1);
 		}
+		current = current->next;
 	}
 	return (0);
 }
