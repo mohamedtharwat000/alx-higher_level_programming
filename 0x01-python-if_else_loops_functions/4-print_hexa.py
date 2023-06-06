430350ce922f00fd828327cab5c96f6ee0ae29c0
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-06-07 01:55:36 +0300

    fix

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{0:02}".format(number))
-    else:
-        print("{0:02}".format(number), end=", ")
+for number in range(0, 99):
+    print("{0} = {1}".format(number, hex(number)))
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print(f"{number} = 0x{number:x}")
+for number in range(0, 100):
+    if number == 99:
+        print("{0:02}".format(number))
+    else:
+        print("{0:02}".format(number), end=", ")
