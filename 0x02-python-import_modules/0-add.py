1dcb47edd1f0b85f10930cb5e84d250d16b16e91
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-08 19:52:23 +0300

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == __main__:
-    from add_0 import add
+from add_0 import add
+if __name__ == "__main__":
     a, b = 1, 2
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
