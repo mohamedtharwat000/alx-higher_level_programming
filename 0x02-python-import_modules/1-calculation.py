1b38fefb1028446ceb05aba7c73036459532bf5e
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-08 20:12:52 +0300

    Create 1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+import calculator_1.py as calculator
+a = 10
+b = 5
+print("{0:d} + {1:d} = {2:d}".format(a, b, calculator.add(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, calculator.sub(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, calculator.mul(a, b)))
+print("{0:d} + {1:d} = {2:d}".format(a, b, calculator.div(a, b)))
