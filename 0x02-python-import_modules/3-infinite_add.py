3404d9bfe01800f80f9418029566b1c013957d6a
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-08 21:10:52 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ from sys import argv
 if __name__ == "__main__":
     result = 0
     for i in range(1, len(argv)):
-        result += int(arg)
+        result += int(argv[i])
     print(f"{result:d}")
