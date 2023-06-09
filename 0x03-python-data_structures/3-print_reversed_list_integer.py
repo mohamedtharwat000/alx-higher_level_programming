8ecf5d9df72c0f4b646164208cdca020b454af13
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-09 20:33:59 +0300

    Create 3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    list_len = len(my_list)
+    for i in range(list_len):
+        print("{0:d}".format(my_list[(list_len - 1) - i]))
