2fe084928e201025c34eae65d03597e8581174a7
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-10 17:51:40 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string_len = len(my_string)
     result = ""
-    for c in range(string_len):
-        result += my_string[c] if my_string[c] != 'c' and my_string[c] != 'C' else ""
+    for c in range(len(my_string)):
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            result += my_string[c]
     return result
