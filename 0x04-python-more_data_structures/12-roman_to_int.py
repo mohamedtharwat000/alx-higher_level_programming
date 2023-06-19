bf6e52b42a81350cdda7bc15c82e45cfb7ea6cd3
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-19 15:52:53 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,8 @@ def roman_to_int(roman_string):
     roman_string_len = len(roman_string)
     result = 0
     for i in range(roman_string_len):
-        if i + 1 < roman_string_len:
-            if numbers[roman_string[i]] < numbers[roman_string[i + 1]]:
-                result -= numbers[roman_string[i]]
+        if i + 1 < roman_string_len and numbers[roman_string[i]] < numbers[roman_string[i + 1]]:
+            result -= numbers[roman_string[i]]
         else:
             result += numbers[roman_string[i]]
     return result
