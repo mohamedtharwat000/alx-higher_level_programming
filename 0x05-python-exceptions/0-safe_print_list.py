52ed22b81e45535bd46ef099ad0e5a9999cc2a65
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-27 15:28:45 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0:
+    if x == 0 or my_list == []:
         return 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-            i += 1
+        i += 1
     except IndexError:
         pass
     finally:
