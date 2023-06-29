f496002b61b7d29ec810fa065bf059dcdd932fab
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-29 21:00:07 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Square class
+"""
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size=0):
+        """
+        init method
+        @size (int): size of the square
+        @return: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
