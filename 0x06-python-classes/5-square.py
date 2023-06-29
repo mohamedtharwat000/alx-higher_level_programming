628464905a98db3d5e87f272e6cb44a6bf0eaf6b
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-06-29 21:12:27 +0300

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+    Square class
+"""
+class Square:
+    """
+    Define a square
+    """
+    def __init__(self, size=0):
+        """
+        init method
+        @size (int): size of the square
+        @return: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        size - method that returns the current square size
+        @return: the current square size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size - method that sets the current square size
+        @value (int): size of the square
+        @return: None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Area - method that returns the current square area
+        @return: the current square area
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Print - method that prints in stdout the square with the character #
+        @return: None
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+            print()
