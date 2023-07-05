15f04a7c2fd3a2947cbe0cc73771d3d090885f8d
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-05 16:36:38 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,15 +13,17 @@ def text_indentation(text):
         Args:
             text (str): text to print.
     """
+    new_line = False
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
             print(text[i])
             print()
+            new_line = True
         else:
-            if text[i] == ' ' and \
-              (text[i-1] == '.' or text[i-1] == '?' or text[i-1] == ':'):
+            if text[i] == ' ' and new_line == True:
                 continue
             else:
                 print(text[i], end='')
+            new_line = False
