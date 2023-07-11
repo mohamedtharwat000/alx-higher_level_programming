5e7ea39b1e2ffb814c33a074b3b015721ad223b7
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-11 14:59:45 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,7 +3,7 @@
 """
     module for the class Square.
 """
-Rectangle = __import__('10-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -19,10 +19,10 @@ class Square(Rectangle):
                 TypeError: if size is not an integer
                 ValueError: if size is <= 0
         """
+        self.integer_validator("size", size)
         self.__size = size
-        self.integer_validator("size", self.__size)
 
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
 
     def area(self):
         """
