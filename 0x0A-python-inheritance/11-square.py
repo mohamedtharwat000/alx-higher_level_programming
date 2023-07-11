ad71a6376dd75d323da14a07f51e2bb781362a54
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-11 06:27:56 +0300

    Create 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('8-rectangle').Rectangle
+"""
+    module for the class Square.
+"""
+
+
+class Square(Rectangle):
+    """
+        class Square
+    """
+    def __init__(self, size):
+        """
+            __init__ - class constructor
+            Args:
+                size (int): size of the square
+            Raises:
+                TypeError: if size is not an integer
+                ValueError: if size is <= 0
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+            area - returns the area of the Square
+            Retuens:
+                area of the Square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+            __str__ - returns a string representation of the Square
+            Returns:
+                string representation of the Square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
