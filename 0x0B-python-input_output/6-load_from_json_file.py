8f2288a03d8065d46cb8ab2af37671902f373a7b
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 04:04:22 +0300

    Create 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+    module for function load_from_json_file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+        load_from_json_file - function that creates an Object from a “JSON file”
+        Args:
+            filename: name of file
+        Return:
+            object
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
