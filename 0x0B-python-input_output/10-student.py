a1d27357994dbf3ae7de2bed0ccd9bd902984f33
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 05:44:44 +0300

    Create 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    module for class Student
+"""
+
+
+class Student:
+    """
+        Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            __init__ - constructor
+            Args:
+                first_name (str): first name
+                last_name (str): last name
+                age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
