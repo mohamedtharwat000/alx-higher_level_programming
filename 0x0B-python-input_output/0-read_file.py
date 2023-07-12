bef090a9d5819d5215949e80428b323d7a854fad
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 03:42:43 +0300

    Update 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    module for function read_file
+"""
+
+
+def read_file(filename=""):
+    """
+        read_file - that reads a text file (UTF8) and prints it to stdout.
+        Args:
+            filename (str): name of the file to be read.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
