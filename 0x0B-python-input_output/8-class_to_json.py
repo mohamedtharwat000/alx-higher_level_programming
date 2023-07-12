f5d564cd3e76281b8be929d0fbd44220fd91ffa3
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 05:24:53 +0300

    Create 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    module for function class_to_json
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+        class_to_json - function that returns the dictionary description with
+        simple data structure (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object:
+    """
+    return json.dumps(obj, default=lambda o: o.__dict__)
