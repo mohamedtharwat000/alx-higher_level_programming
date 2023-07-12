e75925df6cd2bbcbacacf24d6158bf380f22c4d9
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 05:50:21 +0300

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    module for class Student
+"""
+
+
+class Student:
+    """
+        Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+            __init__ - constructor
+            Args:
+                first_name (str): first name
+                last_name (str): last name
+                age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+            reload_from_json - reloads the instance attributes
+            Args:
+                json (dict): json to use
+        """
+        for k, v in json.items():
+            self.k = v
