6d67b53afef5bf15241b7c6d6a969ed0bd9a2bd7
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-12 05:39:09 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,7 +3,6 @@
 """
     module for function class_to_json
 """
-import json
 
 
 def class_to_json(obj):
@@ -12,4 +11,4 @@ def class_to_json(obj):
         simple data structure (list, dictionary, string, integer and boolean)
         for JSON serialization of an object:
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
