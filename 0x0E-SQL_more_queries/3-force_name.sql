767f718c70814d4cfa759b53d79f047aa1f296c4
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 01:29:47 +0300

    Update 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,6 @@
-/* script that creates the table force_name on your MySQL server. */
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+/* script that creates a table. */
+-- Create table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
