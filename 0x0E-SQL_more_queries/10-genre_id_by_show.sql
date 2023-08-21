8d6ae6ed5299b851161bcd00dbf6479dc54b8903
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:15:34 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 /* script that lists all shows contained in hbtn_0d_tvshows that have
 at least one genre linked. */
 
---lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- lists all shows that have at least one genre linked.
 SELECT tv_shows.titles, tv_show_genres.genre_id
   FROM tv_shows
  INNER JOIN tv_shows_genres
