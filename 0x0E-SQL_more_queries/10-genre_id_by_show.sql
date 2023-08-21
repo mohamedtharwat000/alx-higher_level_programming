992904c9652739f95f1d870dbf02cb67215bc088
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:20:05 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,8 @@
 at least one genre linked. */
 
 -- lists all shows that have at least one genre linked.
-SELECT tv_shows.titles, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
- INNER JOIN tv_shows_genres
+ INNER JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
- ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ ORDER BY tv_shows.title ,tv_show_genres.genre_id
