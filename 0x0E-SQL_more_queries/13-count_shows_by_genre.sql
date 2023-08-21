a6f48fbc4b74043094233c353703ce1c577e7091
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:33:03 +0300

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
-/* script that lists all genres from hbtn_0d_tvshows and displays
-the number of shows linked to each. */
+/* script that lists all genres and displays the number of shows
+linked to each. */
 
--- lists all genres from hbtn_0d_tvshows and displays
+-- lists all genres and displays
 SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
   FROM tv_genres
  INNER JOIN tv_show_genres
