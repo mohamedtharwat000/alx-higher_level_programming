50e2dcfd0d9f1f43fc0c2cc4823df2b969771b09
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:23:14 +0300

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 /* script that lists all shows contained in the database hbtn_0d_tvshows. */
 
---list all shows
+-- list all shows
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
   LEFT JOIN tv_show_genres
