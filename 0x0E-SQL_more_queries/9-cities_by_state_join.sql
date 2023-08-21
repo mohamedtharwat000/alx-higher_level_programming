d33512f014d928466148b615ea1b902fe23945f4
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:02:01 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,6 @@
 --list all cities cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states
+INNER JOIN states
 ON cities.state_id = states.id
+ORDER BY cities.id ASC
