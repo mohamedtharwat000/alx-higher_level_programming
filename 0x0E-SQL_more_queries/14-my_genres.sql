7e0b6aa4164cd2fc6c6bd871de2d926e537d3b5c
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:37:48 +0300

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,6 @@
+ /* script that uses the hbtn_0d_tvshows database to lists all genres
+of the show Dexter. */
+
  -- lists all genres of the show Dexter.
 SELECT tv_genres.name FROM tv_genres
  INNER JOIN tv_show_genres
