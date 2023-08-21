69e988e24e6796b94187f8dacf76c70e1ceb0845
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 03:09:53 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 /* script that lists all cities contained in the database hbtn_0d_usa. */
 
---list all cities cities.id - cities.name - states.name
+-- list all cities cities.id , cities.name , states.name
 SELECT cities.id, cities.name, states.name
   FROM cities
  INNER JOIN states
