173e8ad26f1643c60bc0aecc46f0105406518e50
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 22:29:11 +0300

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -11,11 +11,4 @@ from the database hbtn_0d_tvshows.
         ON tv_shows.id = tv_show_genres.show_id
  LEFT JOIN tv_genres
         ON tv_genres.id = tv_show_genres.genre_id
-     UNION
-    SELECT tv_shows.title AS `title`, tv_genres.name AS `genre`
-      FROM tv_shows
-RIGHT JOIN tv_show_genres
-        ON tv_shows.id = tv_show_genres.show_id
-RIGHT JOIN tv_genres
-        ON tv_genres.id = tv_show_genres.genre_id
   ORDER BY `title`, `genre` ASC;
