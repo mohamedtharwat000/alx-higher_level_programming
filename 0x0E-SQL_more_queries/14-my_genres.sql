5d12b31ff41bdda739b9cdfa4382dc33c9d6fb21
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-21 21:57:22 +0300

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,7 +9,7 @@ SELECT tv_genres.name
  INNER JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
  WHERE tv_show_genres.show_id = (
-         SELECT id
+         SELECT tv_shows.id
            FROM tv_shows
           WHERE tv_shows.title = 'Dexter')
  ORDER BY tv_genres.name ASC;
