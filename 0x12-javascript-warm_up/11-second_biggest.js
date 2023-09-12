25d122113cc76604b059fb0d9414ff72e3dcc68b
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-12 03:19:02 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 let max = 0;
+if (args.length <= 3) {
+  console.log(max);
+  process.exit();
+}
 for (const arg of args) {
   if (arg > max) {
     max = arg;
