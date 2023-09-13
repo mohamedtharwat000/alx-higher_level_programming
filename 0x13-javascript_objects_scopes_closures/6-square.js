5e47b93500e4bf17c68f38fbd9225c56a3413065
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-13 04:42:06 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const square = require('./5-square.js');
 class Square extends square {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c) {
     const char = c ?? 'X';
     for (let i = 0; i < this.height; i++) {
