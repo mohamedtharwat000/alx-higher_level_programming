34a0dac7f2270e7193d5699f9d57c66b13039d61
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-19 23:02:00 +0300

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""displays all values in the states table where name matches the argument."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = connection.cursor()
+    cursor.execute("""
+                   SELECT * FROM states
+                   WHERE {name} LIKE BINARY 'N%'
+                   ORDER BY states.id ASC
+                   """.format(name=state_name))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    connection.close()
