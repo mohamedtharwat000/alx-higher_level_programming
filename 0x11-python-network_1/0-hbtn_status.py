300e1729731567c12ab82ed6cb226341d7e8fd34
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-30 01:29:06 +0300

    Create 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Python script that fetches https://alx-intranet.hbtn.io/status."""
+
+import urllib
+
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body_response = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body_response)}")
+        print(f"\t- content: {body_response}")
+        print(f"\t- utf8 content: {body_response.decode('utf-8')}")
