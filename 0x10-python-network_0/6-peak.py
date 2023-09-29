7504799509f1a86ce3a86621de4f7bacbcc85284
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-29 05:01:24 +0300

    Create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""function that finds a peak in a list of unsorted integers.""
+
+def find_peak(list_of_integers):
+    ""function that finds a peak in a list of unsorted integers.""
+    return max(list_of_integers)
