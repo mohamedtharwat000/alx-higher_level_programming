3107c8db1a55223f795f86e0de9e081dce50dcff
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-29 05:09:48 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,4 @@
 
 def find_peak(list_of_integers):
     ""function that finds a peak in a list of unsorted integers.""
-    if len(list_of_integers) == 0:
-        return None
-    return max(list_of_integers)
+    return sorted(list_of_integers)[-1]
