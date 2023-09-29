172722254c997b7acf91412352707ed35263c358
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-09-30 01:50:45 +0300

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+script that takes in a URL, sends a request to the URL and displays the body of
+the response (decoded in utf-8).
+"""
+
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("fError code: {error.code}")
