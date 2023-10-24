41e25b4d1287461e7f9f9e9b2897122c105ff981
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-24 19:48:55 +0300

    Create 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const wedgeAntillesMovies = data.results.filter((movie) =>
+      movie.characters.includes('18')
+    );
+    console.log(wedgeAntillesMovies.length);
+  }
+});
