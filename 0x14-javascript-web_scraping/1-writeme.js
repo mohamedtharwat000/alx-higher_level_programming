4116ed5793fac2adeb428b6f9ac682a960914e68
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-24 19:32:01 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,7 +8,5 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log('String successfully written to the file.');
   }
 });
