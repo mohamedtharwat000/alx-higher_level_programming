9a3728a88df95a6ea2b92d1bebe0390339ff66af
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-24 20:26:24 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,13 +7,11 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    const wedgeAntillesMovies = data.results.filter((movie) =>
-      movie.characters.includes(
-        'https://swapi-api.alx-tools.com/api/people/18/'
-      )
-    );
-    console.log(wedgeAntillesMovies.length);
+    return;
   }
+  const data = JSON.parse(body);
+  const wedgeAntillesMovies = data.results.filter((movie) =>
+    movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+  );
+  console.log(wedgeAntillesMovies.length);
 });
