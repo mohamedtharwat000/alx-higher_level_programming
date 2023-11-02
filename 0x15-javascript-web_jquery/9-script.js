63f5a7888bb7c816983ef9fcb61f2fb786024aa2
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-02 16:48:51 +0200

    Create 9-script.js

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$.ajax({
+		url: "https://hellosalut.stefanbohacek.dev/?lang=fr",
+		method: 'GET',
+		success: function (data) {
+			$('DIV#hello').text(data.hello);
+		}
+	});
+});
