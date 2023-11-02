5c436c307583ad97b9d6872779b3c63cfaa68e04
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-02 16:48:47 +0200

    Create 7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(function(){
+	$.ajax({
+		url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+		method: 'GET',
+		success: function (data) {
+			$('div#character').text(data.name);
+		}
+	});
+});
